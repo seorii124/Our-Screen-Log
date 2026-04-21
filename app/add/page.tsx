85c5f4d6152ba4183f3ab330594d005c6c119826
seorii124 <seorii40@gmail.com>
2026-04-22 01:04:33 +0900

    fix: import error

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { createBrowserClient } from '@supabase/ssr'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useRouter } from 'next/navigation'
 
 export default function AddWorkPage() {
